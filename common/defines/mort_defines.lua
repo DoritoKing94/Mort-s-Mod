10d2e270f90f99e518bc411096f5753834e4a7e7
DoritoKing94 <59567848+DoritoKing94@users.noreply.github.com>
2025-05-07 00:35:57 +0100

    5/5/25
    .

--- common/defines/mort_defines.lua
+++ common/defines/mort_defines.lua
@@ -22,15 +22,15 @@ NDefines.NProduction.MIN_LICENSE_ACTIVE_DAYS = 1
 
 NDefines.NProduction.LICENSE_IC_COST_YEAR_INCREASE = 0
 
---Speed - Original { 2.0, 0.5, 0.2, 0.1, 0.0 } 	#Bogs?{ 0.5, 0.13, 0.08, 0.04, 0.0 }
+--Speed - Original { 2.0, 0.5, 0.2, 0.1, 0.0 } 	#Bog{ 0.5, 0.13, 0.08, 0.04, 0.0 }
 NDefines.NGame.GAME_SPEED_SECONDS = { 5.0, 0.19, 0.14, 0.09, 0.0 }
 
 --Logistic Bombing *ban
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_NO_TRUCK_DISRUPTION_FACTOR = 0.0001 -- If a unit isn't motorized, still disrupt its supply by damage * this
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_DAMAGE_FACTOR = 0.0001
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_INFRA_DAMAGE_SPILL_FACTOR = 0.0001 -- Portion of truck damage to additionally deal to infrastructure
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_FACTOR = 0.0001
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_DISRUPTION_MITIGATION = 0.0001 -- Multiply Train Damage by (Smooth / (Smooth + (Disruption * Mitigation)))
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_RAILWAY_DAMAGE_SPILL_FACTOR = 0.0001 -- Portion of train damage to additionally deal to railways
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_NO_TRUCK_DISRUPTION_FACTOR = 0.00001 -- If a unit isn't motorized, still disrupt its supply by damage * this
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_DAMAGE_FACTOR = 0.00001
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_INFRA_DAMAGE_SPILL_FACTOR = 0.00001 -- Portion of truck damage to additionally deal to infrastructure
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_FACTOR = 0.00001
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_DISRUPTION_MITIGATION = 0.00001 -- Multiply Train Damage by (Smooth / (Smooth + (Disruption * Mitigation)))
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_RAILWAY_DAMAGE_SPILL_FACTOR = 0.00001 -- Portion of train damage to additionally deal to railways
 
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_DIRECT_DISRUPTION_DAMAGE_FACTOR = 0.0001 -- Disruption damage to supply throughput done by bombing damage, not dependant on killing trains which also causes diruption.
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_DIRECT_DISRUPTION_DAMAGE_FACTOR = 0.00001 -- Disruption damage to supply throughput done by bombing damage, not dependant on killing trains which also causes diruption.
